c0b5029b4ab0c56c3c508ec0271f9355b730def9
entropy <alexandrisanas@gmail.com>
2018-08-31 00:44:59 +0300

    init

---
+++ src/components/FullPost/FullPost.js
@@ -0,0 +1,55 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+import './FullPost.css';
+
+class FullPost extends Component {
+
+    state = {
+        loadedPost: null
+    }
+
+    componentDidUpdate () {
+
+        if (this.props.id) {
+            if ( !this.state.loadedPost || (this.state.loadedPost && this.state.loadedPost.id !== this.props.id)) {
+                axios.get(`https://jsonplaceholder.typicode.com/posts/${this.props.id}`)
+                        .then( res => {
+                            this.setState({
+                                loadedPost: res.data
+                            })
+                    })
+            }
+
+        }
+
+    }
+
+    render () {
+        const { loadedPost } = this.state;
+        let post = <p style={{ textAlign: 'center' }}>Please select a Post!</p>;
+
+        if (this.props.id) {
+             post = <p style={{ textAlign: 'center' }}>Loading...</p>;
+        }
+
+        if( this.state.loadedPost ) {
+
+            post = (
+                <div className="FullPost">
+                    <h1>{loadedPost.title}</h1>
+                    <p>{loadedPost.body}</p>
+                    <div className="Edit">
+                        <button className="Delete">Delete</button>
+                    </div>
+                </div>
+
+            );
+        }
+
+
+        return post;
+    }
+}
+
+export default FullPost;
